40fd087db264f6bb02c748036f878f3b5d4e324c
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-05-20 14:33:09 +0900

    Mod layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top#index'
 
-  get 'packages/:id', to: 'packages#show'
+  get '/packages/:id', to: 'packages#show', as: 'package'
   get '/search', to: 'top#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
