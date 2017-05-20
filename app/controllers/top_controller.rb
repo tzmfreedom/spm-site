c1c90e6dba6916ef1b01b7acbd754bb05b9d6884
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-05-20 14:32:45 +0900

    Mod search condition

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,15 @@
 class TopController < ApplicationController
 
-  PER_PAGE = 1
+  PER_PAGE = 30
 
   def index
   end
 
   def search
+    q = "%#{params[:q]}%"
     @packages = Package.eager_load(:package_items)
-                       .where('packages.description like ?', "%#{params[:q]}%").page(params[:page]).per(PER_PAGE)
+                       .where('packages.description like ? or name like ?', q, q)
+                       .order(stargazers_count: :desc)
+                       .page(params[:page]).per(PER_PAGE)
   end
 end
